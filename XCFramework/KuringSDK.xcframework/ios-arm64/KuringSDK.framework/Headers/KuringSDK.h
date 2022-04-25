00d3325a1bd87e11f4381e1c1b3ab87c71fc22b0
Jaesung Lee <jaesung.lee@sendbird.com>
2022-04-25 23:34:57 +0900

    [버전] 1.1.2 배포

---
+++ XCFramework/KuringSDK.xcframework/ios-arm64/KuringSDK.framework/Headers/KuringSDK.h
@@ -0,0 +1,18 @@
+//
+//  KuringSDK.h
+//  KuringSDK
+//
+//  Created by Jaesung Lee on 2021/11/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KuringSDK.
+FOUNDATION_EXPORT double KuringSDKVersionNumber;
+
+//! Project version string for KuringSDK.
+FOUNDATION_EXPORT const unsigned char KuringSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KuringSDK/PublicHeader.h>
+
+
